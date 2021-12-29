f1b9f9155a79a78005c4e1bbe8212f6d74cb5758
David Inglis <davidcinglis@gmail.com>
2021-12-28 20:42:53 -0800

    added tooltip to copy button

--- src/static/js/form.js
+++ src/static/js/form.js
@@ -1,31 +1,33 @@
 $(document).ready(function () {
-    $("#decklist-form").submit(function (event) {
-      $("#form-submit").attr("disabled", true);
-      $("#button-loaded").attr("hidden", true);
-      $("#button-loading").attr("hidden", false);
-      var formData = {
-        url: $("#url").val(),
-        format: $("#format").val(),
-      };
-  
-      $.ajax({
-        type: "GET",
-        url: "/decklist/",
-        data: formData,
-        dataType: "json",
-        encode: true,
-      }).done(function (data) {
-        $("#decklist-text").text(data.decklist);
-        $("#form-submit").attr("disabled", false);
-        $("#decklist").attr("hidden", false);
-        $("#button-loading").attr("hidden", true);
-        $("#button-loaded").attr("hidden", false);
-      });
-  
-      event.preventDefault();
+
+  $('#copy-decklist').tooltip();  
+  $("#decklist-form").submit(function (event) {
+    $("#form-submit").attr("disabled", true);
+    $("#button-loaded").attr("hidden", true);
+    $("#button-loading").attr("hidden", false);
+    var formData = {
+      url: $("#url").val(),
+      format: $("#format").val(),
+    };
+
+    $.ajax({
+      type: "GET",
+      url: "/decklist/",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (data) {
+      $("#decklist-text").text(data.decklist);
+      $("#form-submit").attr("disabled", false);
+      $("#decklist").attr("hidden", false);
+      $("#button-loading").attr("hidden", true);
+      $("#button-loaded").attr("hidden", false);
     });
 
-    $('#copy-decklist').click(function() {
-      navigator.clipboard.writeText($('#decklist').text());
-    });
+    event.preventDefault();
   });
+
+  $('#copy-decklist').click(function() {
+    navigator.clipboard.writeText($('#decklist').text());
+  });
+});
